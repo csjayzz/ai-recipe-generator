993f015385fb85f40df7fd981250caec892f01ec
jayesh <jayeshj0304@gmail.com>
2026-03-12 18:49:36 +0530

    implemented updateProfile function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,17 +7,34 @@ import UserPreference from "../models/UserPreference";
 Get user profile
 */
 export const getProfile = async (req, res, next) => {
-try {
-const user = await User.findById(req.user.id);
-const preferences = await UserPreference.findByUserId(req.user.id);
-res.json({
-success: true,
-data: {
-user,
-preferences
-}
-});
-} catch (error) {
-next(error);
-}
+  try {
+    const user = await User.findById(req.user.id);
+    const preferences = await UserPreference.findByUserId(req.user.id);
+    res.json({
+      success: true,
+      data: {
+        user,
+        preferences,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Update user profile
+ */
+export const updateProfile = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.update(req.user.id, { name, email });
+    res.json({
+      success: true,
+      message: "Profile updated successfully",
+      data: { user },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
